b095f7f28b0d3e55f97fda7ac7c6c37b357693e6
Moriwaki <mttfmh@icloud.com>
2023-11-24 22:20:35 +0900

    Initial commit

---
+++ src/main/java/com/moriwaki/Java9thTopic/Java9thTopicApplication.java
@@ -0,0 +1,13 @@
+package com.moriwaki.Java9thTopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java9thTopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java9thTopicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moriwaki/Java9thTopic/Java9thTopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moriwaki.Java9thTopic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java9thTopicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
